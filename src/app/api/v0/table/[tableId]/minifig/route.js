4160d14ae35b8ee6e2943b6be00c490d1b8602e9
Clomby <clomby@metrix.pw>
2025-04-22 15:35:37 +0200

    Renamed old API to v0, likely removed when v1 works as expected, keeping as reference

--- src/app/api/v0/table/[tableId]/minifig/route.js
+++ src/app/api/v0/table/[tableId]/minifig/route.js
@@ -211,20 +211,3 @@ export async function POST(req, { params }) {
   }
 }
 
-// src/app/api/v1/table/[tableId]/minifig/route.js
-
-// import minifigController from '@/lib/API/Controllers/v1/MinifigController';
-
-// /**
-//  * GET handler for retrieving all minifigs in a table
-//  */
-// export const GET = minifigController.withErrorHandling(
-//   minifigController.get.bind(minifigController)
-// );
-
-// /**
-//  * POST handler for adding minifigs to a table
-//  */
-// export const POST = minifigController.withErrorHandling(
-//   minifigController.post.bind(minifigController)
-// );
